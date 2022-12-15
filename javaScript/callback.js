9c8a8596f6618dd4aef258e3a922293d68f5f555
Rahulverma0711 <vermamm122@gmail.com>
2022-12-15 19:46:34 +0530

    callback basics done

--- javaScript/callback.js
+++ javaScript/callback.js
@@ -1,1 +1,11 @@
 // We can pass even function as an argument 
+
+function fun1(){
+    console.log("hello from fun1");
+}
+
+function fun(callback){
+    console.log(callback);// ->this will output a function .
+     callback();
+}
+fun(fun1);
