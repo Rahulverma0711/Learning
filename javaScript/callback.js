bde33fae4c9f2633e7215e1ef5b8607266247b88
Rahulverma0711 <vermamm122@gmail.com>
2022-12-15 22:04:45 +0530

    callback done

--- javaScript/callback.js
+++ javaScript/callback.js
@@ -1,4 +1,4 @@
-// We can pass even function as an argument 
+// We can pass even function as an argument .The function which is parameter is called as a CALLBACK FUNCTION.
 
 function fun1(){
     console.log("hello from fun1");
@@ -6,6 +6,6 @@ function fun1(){
 
 function fun(callback){
     console.log(callback);// ->this will output a function .
-     callback();
+     callback(); // callback function call 
 }
 fun(fun1);
